34a7eb06140894c2e16c9049556f7d3bca84c753
Mark Farnsworth <farnsworth2008@gmail.com>
2023-01-26 19:35:09 +0000

    Misc

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ locals {
 }
 
 module "name" {
-  source = "github.com/s3d-club/terraform-external-name?ref=v1.0.1"
+  source = "github.com/s3d-club/terraform-external-name?ref=v1.0.0"
 
   context = var.name_prefix
   path    = path.module
